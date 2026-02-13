f22c9c9434f3055aadecda788cbe0e4b0d12d0ac
kofdmds-ui <kofdmds@gmail.com>
2026-02-13 15:30:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW Kaya love na love po kita eh. 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Yieeeh, kakilig naman po.",              // Shows when they go past 1000%
+        normal: "Ganyan lang???"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
